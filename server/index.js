851d1bd19ba46944541014f00fbcd3d0260b734e
Derrick Marshall <marshallderrickdnm@gmail.com>
2020-01-08 14:38:17 -0800

    proxy done next adding service to ec2

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+const newrelic = require('newrelic');
+
 const express = require('express');
 const proxy = require('http-proxy-middleware');
 const cors = require('cors');
@@ -7,53 +9,45 @@ const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 // serve up static file
-app.use('/*', express.static('public'));
-
-
-
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
-});
-
 
 const reviews = {
   target: 'http://localhost:3003',
   changeOrigin: true,
-  onProxyRes: function (proxyRes) {
-    proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-  }
 };
 
-const review = proxy(reviews);
-app.use('/', review);
+const reviewProxy = proxy(reviews);
+app.use('/listing/:id', reviewProxy);
 
-const reservations = {
-    target: 'http://3.135.103.1',
-    changeOrigin: true,
-    onProxyRes: function (proxyRes) {
-        proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-      }
-  };
-  const reservation = proxy(reservations);
-  app.use('/', reservation);
+app.use('/*', express.static('public'));
+app.listen(PORT, () => {console.log(`Listening on port ${PORT}...`)});
 
-  const suggestions = {
-    target: 'http://localhost:3001',
-    changeOrigin: true,
-    onProxyRes: function (proxyRes) {
-        proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-      }
-  };
-  const suggestion = proxy(suggestions);
-  app.use('/', suggestion);
+// const reservations = {
+//     target: 'http://3.135.103.1',
+//     changeOrigin: true,
+//     onProxyRes: function (proxyRes) {
+//         proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+//       }
+//   };
+//   const reservation = proxy(reservations);
+//   app.use('/', reservation);
 
-  const photogalleries = {
-    target: 'http://localhost:3004',
-    changeOrigin: true,
-    onProxyRes: function (proxyRes) {
-        proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-      }
-  };
-  const photogallery = proxy(photogalleries);
-  app.use('/', photogallery);
+//   const suggestions = {
+//     target: 'http://localhost:3001',
+//     changeOrigin: true,
+//     onProxyRes: function (proxyRes) {
+//         proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+//       }
+//   };
+//   const suggestion = proxy(suggestions);
+//   app.use('/', suggestion);
+
+//   const photogalleries = {
+//     target: 'http://localhost:3004',
+//     changeOrigin: true,
+//     onProxyRes: function (proxyRes) {
+//         proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+//       }
+//   };
+//   const photogallery = proxy(photogalleries);
+//   app.use('/', photogallery);
 
